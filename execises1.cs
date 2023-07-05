62abe1d53d87031b3ce88b051fbd46630e3a26dd
Mikail Sobolev <ya@hsov.ru>
2023-07-05 19:44:06 +0600

    bag

---
+++ execises1.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 > number2) 
+{
+    Console.Write($"Число {number1} больше числа {number2}");
+}
+else;
+{
+    Console.Write($"Число {number2} больше числа {number1}");
+}
