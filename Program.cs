46d84d11eb706368bc6e588e2ec1fff7da714729
Mikail Sobolev <ya@hsov.ru>
2023-07-05 19:29:28 +0600

    сделал задачу найти максимальное число из двух

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 > number2) 
+{
+    Console.Write($"Число {number1} больше числа {number2}");
+}
+else;
+{
+    Console.Write($"Число {number2} больше числа {number1}");
+}
