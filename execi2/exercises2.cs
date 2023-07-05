72079f784b039c0d12ef36665832a8fc28e05259
Mikail Sobolev <ya@hsov.ru>
2023-07-05 20:04:50 +0600

    bag

---
+++ execi2/exercises2.cs
@@ -0,0 +1,18 @@
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+if(number1 > number2 && number1 > number3)
+{
+    Console.Write($"Самое большое число {number1}");
+}
+else if(number2 > number3)
+{
+    Console.Write($"Самое большое число {number2}");
+}
+else;
+{
+    Console.Write($"Самое большое число {number3}");
+}
