7cb1be00d56eaa0c0674a482e8a29cb8fbd943f2
Mikail Sobolev <ya@hsov.ru>
2023-07-06 23:32:26 +0600

    РЕШЕНИЕ 6 ЗАДАЧИ

---
+++ execi3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+if((number1 % 2) == 0)
+{
+    Console.Write("четное ");
+}
+else;
+ {
+    Console.Write("не четное");
+ }
